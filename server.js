cb6faf54ea49ba0e976f34680c35d2bbb959eb22
Jeremiah <jeremiaht.walters@gmail.com>
2017-03-08 15:17:53 -0800

    new routes and test written.

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const cors = require('cors');
 const morgan = require('morgan');
 const debug = require('debug')('cfgram:server');
 const mongoose = require('mongoose');
-const Promise = require('bluebird');
 const dotenv = require('dotenv');
 
 const authRouter = require('./route/auth-router.js');
 const galleryRouter = require('./route/gallery-router.js');
+const flickRouter = require('./route/flick-router.js');
 const errors = require('./lib/error-middleware.js');
 
-dotenv.load();
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+dotenv.load();
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -24,8 +24,11 @@ app.use(morgan('dev'));
 
 app.use(authRouter);
 app.use(galleryRouter);
+app.use(flickRouter);
 app.use(errors);
 
-app.listen(PORT, () => {
+const server = module.exports = app.listen(PORT, () => {
   debug(`server up: ${PORT}`);
 });
+
+server.isRunning = true;
