8fa79bd30f1ee015e9fbf00fe5c5cb7fefa3a3ae
Jeremiah <jeremiaht.walters@gmail.com>
2017-03-09 10:28:43 -0800

    testing branch

--- model/flick.js
+++ model/flick.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//Schema
 const flickSchema = Schema({
   name: { type: String, required: true },
   desc: { type: String, required: true },
